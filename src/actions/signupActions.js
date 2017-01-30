e60bc7062cbaa2a054ab039c52b2194c6328bbce
Tim <timothee.astier@essec.edu>
2017-01-30 17:49:40 +0100

    Removes API_URL test

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import API_URL from '../config/api';
 
-console.log('API_URL: ' + API_URL);
-
 export function userSignupRequest(userData) {
   return dispatch => {
     return axios.post(`${API_URL}/api/users`, userData);
