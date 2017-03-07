0c250af840d3b91f9102235b98399045315a7976
Tim <timothee.astier@essec.edu>
2017-03-07 14:29:39 +0100

    test

--- src/textEditor/containers/TextEditor.js
+++ src/textEditor/containers/TextEditor.js
@@ -27,6 +27,7 @@ class TextEditor extends React.Component {
 
   // TODO: Switch to readonly when isSaving
   render() {
+    console.log(DEBUG);
     return (
       <div id="text-editor">
         <h2><Label basic color="black" className="main-label">课文</Label></h2>
